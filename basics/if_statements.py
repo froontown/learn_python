999456bb9a053cf7437cda46c8a9d93cb33188df
froontown <froontown@froontowns-MacBook-Pro.local>
2017-03-19 13:38:45 -0400

    onto booleans

--- basics/if_statements.py
+++ basics/if_statements.py
@@ -6,3 +6,29 @@ for car in cars:
 		print(car.upper())
 	else:
 		print(car.title())
+
+# Can do things to set the case or something in case it doesn't amtter:
+a = 'AAA'
+a.lower() == 'aaa'
+# returns True
+
+# Other checks:
+# ==, !=, <=, >=
+
+# `and` is the same as `&&`
+age_0 = 22
+age_1 = 18
+age_0 >= 22 and age_1 <= 21 # Returns true
+
+# `or` is the same as `||`
+age_0 >=21 or age_1 >= 21 # Returns true
+
+# `in` and `not in` is the same as `.include?`
+'accord' in cars # returns True
+
+'4runner' not in cars # returns True
+suv = '4runner'
+
+if suv not in cars:
+	print(suv.title() + ' is not in cars') # 4Runner is not in cars
+
